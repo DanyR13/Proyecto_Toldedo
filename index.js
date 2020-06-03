71172f41bad4403c36deabe72be04be1ba06c7aa
DanyR13 <65319614+DanyR13@users.noreply.github.com>
2020-06-03 14:57:25 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+
+const PUERTO = process.env.PORT || 8080;
+
+let pintoresRouter = require('./routes/pintor');
+const app = express();
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', pintoresRouter);
+
+mongoose.Promise = global.Promise;
+const cadena = 'mongodb+srv://admin:admin@riveracarlos-ha1hz.mongodb.net/pintores?retryWrites=true&w=majorityCopy';
+mongoose.connect(cadena, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    console.log('Conexión establecida');
+})
+.catch(err=> console.log(err));
+
+app.listen(PUERTO, ()=>{
+    console.log('Escuchando el puerto...');
+});
+
---
+++ routes/pintor.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+let pintor = require('../controllers/pintoresController.js');
+
+let router = express.Router();
+
+router.get('/', pintor.list);
+
+module.exports = router;
+
